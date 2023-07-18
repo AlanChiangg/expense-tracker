83843d46c10af296d7d0daae46d2078d9d221b83
AlanChiangg <alaln35alan@gmail.com>
2023-07-18 16:26:03 +0800

    feat: add edit page

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+//create
 router.get('/new', (req, res) => {
   res.render('new')
+})
+
+router.post('/new', (req, res) => {
+  
+})
+
+//edit
+router.get('/edit', (req, res) => {
+  res.render('edit')
+})
+
+router.post('/edit', (req, res) => {
+
 })
 
 module.exports = router
