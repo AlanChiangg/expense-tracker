f3610fa2badc5532aba11d7b80b6bef52687d8ee
AlanChiangg <alaln35alan@gmail.com>
2023-07-19 14:31:15 +0800

    feat: add user model

--- models/record.js
+++ models/record.js
@@ -14,14 +14,16 @@ const recordSchema = new Schema({
     min: 0,
     required: true
   },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    required: true
+  },
   categoryId: {
     type: Schema.Types.ObjectId,
     ref: "Category",
     required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
 })
 
