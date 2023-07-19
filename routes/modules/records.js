de238382cae40ab4b02481f66be3a47b0f960659
AlanChiangg <alaln35alan@gmail.com>
2023-07-19 16:40:01 +0800

    feat: add create record function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,14 +2,36 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const User = require('../../models/user')
 
 router.get('/new', (req, res) => {
-  res.render('new')
+  Category.find()
+    .lean()
+    .then(categories => {
+      res.render("new", { categories })
+    })
 })
 
 //create
 router.post('/new', (req, res) => {
-  
+  const userId = req.user._id
+  const { name, date, categoryName, amount } = req.body
+  Category.findOne({ name: categoryName })
+    .lean()
+    .then(category => {
+      // 從 Category 取得 categoryId，再新增資料
+      let categoryId = category._id
+
+      Record.create({
+        name,
+        date,
+        amount,
+        userId,
+        categoryId
+      })
+    })
+    .then(() => res.redirect("/"))
+    .catch(error => console.log(error))
 })
 
 router.get('/edit', (req, res) => {
